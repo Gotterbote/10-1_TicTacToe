b21228fc6cf46cd020707e0ec65365ef612a0f78
Gotterbote <got1ssei@msn.com>
2022-03-28 20:24:31 -0500

    Add project files.

---
+++ 10-1_TicTacToe/Models/Cell.cs
@@ -0,0 +1,10 @@
+﻿namespace _10_1_TicTacToe.Models
+{
+    public class Cell
+    {
+        public string Id { get; set; }
+        public string Mark { get; set; }
+        public bool IsBlank => string.IsNullOrEmpty(Mark);
+        public bool IsEndCell => Id.EndsWith("Right");
+    }
+}
---
+++ 10-1_TicTacToe/Models/TicTacToeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace _10_1_TicTacToe.Models
+{
+    public class TicTacToeViewModel
+    {
+        public List<Cell> Cells { get; set; }
+        public Cell Selected { get; set; }
+        public bool IsGameOver { get; set; }
+    }
+}
